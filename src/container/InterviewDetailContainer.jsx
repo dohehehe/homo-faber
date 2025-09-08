28efd38b9d5f714e82915c4cea2476ddd81d2e77
dohehehe <doheerlob@gmail.com>
2025-09-08 20:38:02 +0900

    💄 style: InterviewDetailContainer 스타일 업데이트

--- src/container/InterviewDetailContainer.jsx
+++ src/container/InterviewDetailContainer.jsx
@@ -1,208 +1,115 @@
 'use client';
 
-import styled from '@emotion/styled';
-import { useEffect, useRef, useState } from 'react';
+import { useState, useLayoutEffect } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 import { useInterviewDetail } from '@/hooks/useInterviews';
 import EditorInterviewRender from '@/components/interview/EditorInterviewRenderer';
 import { useCustomScrollbar } from '@/hooks/useCustomScrollbar';
-import { motion, AnimatePresence } from 'motion/react';
+import { AnimatePresence } from 'motion/react';
 import CustomScrollbar from '@/components/common/CustomScrollbar';
-
-const DetailWrapper = styled(motion.main)`
-  width: calc(80vw - 46px);
-  height: 100dvh;
-  padding: 0px 10px 20px 50px;
-  background-color: #F7F7F7;
-  position: absolute;
-  right: ${(props) => props.right};
-  top: 0px;
-  z-index: 4;
-  box-shadow: -2px 0 4px 0 rgba(84,84,84,0.57);
-  display: flex;
-  flex-direction: column;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  padding-left: 60px;
-  padding-right: 130px;
-  padding-bottom: 100px;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-`
-const DetailPageName = styled.h1`
-  font-family: var(--font-gothic);
-  font-size: 1rem;
-  font-weight: 700;
-  letter-spacing: 0.3rem;
-  position: sticky;
-  transform: rotate(90deg);
-  transform-origin: top left;
-  top: 17px;
-  margin-left: -29px;
-`
-
-const InterviewHeader = styled.div`
-  display: flex;
-  gap: 2dvw;
-  margin-top: 12px;
-`
-
-const InterviewTitle = styled.div`
-  font-family: var(--font-gothic);
-  display: flex;
-  flex-direction: column;
-  gap: 14px;
-  margin-top: -5px;
-  text-align: center;
-`
-const InterviewStore = styled.h2`
-  font-weight: 800;
-  font-size: 4.3dvw;
-  background-color:rgb(146, 146, 146);
-  background-clip: text;
-  -webkit-background-clip: text;
-  color: transparent;
-  text-shadow: 3px 4px 5px rgba(245, 245, 245, 0.3), -0.1px -0.1px 6px rgba(100,92,92,0.2), 2px 2px 2px rgba(255,255,255,0.5);
-  position: relative;
-  padding-top: 7px;
-  transition: text-shadow .4s ;
-
-  &:hover{
-      text-shadow: 0px 2px 3px rgba(221, 221, 221, 0.8), -0.1px -0.1px 5px rgba(100,92,92,0.6), 2px 2px 10px rgba(255,255,255,0.5);
-  }
-`
-const InterviewPerson = styled.h3`
-  font-weight: 800;
-  font-size: 1.6dvw;
-  padding-top: 4px;
-  background-color:rgb(145, 145, 145);
-  background-clip: text;
-  -webkit-background-clip: text;
-  color: transparent;
-  text-shadow: 3px 4px 5px rgba(245, 245, 245, 0.3), -0.1px -0.1px 6px rgba(100,92,92,0.2), 2px 2px 2px rgba(255,255,255,0.5);
-`
-const InterviewIntro = styled.h4`
-  font-weight: 600;
-  font-size: 1.3rem;
-  line-height: 1.75;
-  word-break: keep-all;
-  padding: 0 20px 0 20px;
-  text-align: center;
-  color:rgb(34, 34, 34);
-  position: absolute;
-  bottom: 30px;
-  width: 80%;
-  // left: 0;
-  margin: 0 auto;
-  mix-blend-mode: multiply;
-  // text-shadow: 0px 0px 7px rgba(255,255,255,1);
-`
-
-const InterviewCoverImg = styled.img`
-  height: 100.2dvh;
-  width: auto;
-  margin-right: -130px;
-  margin-left: auto;
-  // margin-top: -93px;
-  object-fit: contain;
-  border-radius: 0% 0 0% 43%;
-  margin-top: -26px;
-`
-
-const InterviewInfo = styled.div`
-  font-weight: 500;
-  font-size: 1.1rem;
-  margin-bottom: 8px;
-  margin-left: auto;
-
-  & :first-of-type{
-    margin-top: 200px;
-  }
-`
-
-const LoadingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  font-family: var(--font-gothic);
-  font-size: 1.5rem;
-`;
-
-const ErrorContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  font-size: 1.5rem;
-`;
+import useWindowSize from '@/hooks/useWindowSize';
+import * as S from '@/styles/interview/interviewDetailContainer.style';
+import Link from 'next/link';
 
 function InterviewDetailContainer({ }) {
-  const router = useRouter();
   const pathname = usePathname();
+  const { isMobile, isReady } = useWindowSize();
   const [right, setRight] = useState('-100dvw');
+  const [bottom, setBottom] = useState('-100dvh');
   const interviewId = pathname.startsWith('/interview/') && pathname !== '/interview' ? pathname.split('/')[2] : null;
   const { interview, isLoading, error } = useInterviewDetail(interviewId);
   const { containerRef, scrollState, scrollToRatio } = useCustomScrollbar();
 
-  // pathname 변경 시 width 업데이트
-  useEffect(() => {
-    const newRight = pathname.startsWith('/interview/') && pathname !== '/interview' ? '0px' : '-100dvw';
-    setRight(newRight);
-  }, [pathname]);
+  // 모바일에서 사용할 bottom 위치를 계산하는 함수
+  const getMobileBottomPosition = (pathname) => {
+    if (pathname.startsWith('/interview/') && pathname !== '/interview') {
+      return '0px';
+    } else {
+      return '-100dvh';
+    }
+  };
+
+  // 데스크톱에서 사용할 right 위치를 계산하는 함수
+  const getDesktopRightPosition = (pathname) => {
+    if (pathname.startsWith('/interview/') && pathname !== '/interview') {
+      return '0px';
+    } else {
+      return '-100dvw';
+    }
+  };
+
+  // pathname 변경 시 위치 업데이트
+  useLayoutEffect(() => {
+    // isReady가 false면 아직 초기값이 설정되지 않았으므로 실행하지 않음
+    if (!isReady) return;
+
+    if (isMobile) {
+      const newBottom = getMobileBottomPosition(pathname);
+      setBottom(newBottom);
+    } else {
+      const newRight = getDesktopRightPosition(pathname);
+      setRight(newRight);
+    }
+  }, [pathname, isMobile, isReady]);
+
+  // isReady가 false면 로딩 상태 표시
+  if (!isReady) {
+    return null;
+  }
 
   return (
     <AnimatePresence mode="wait">
       {interviewId && (
-        <DetailWrapper
+        <S.DetailWrapper
           key={interviewId}
           ref={containerRef}
           right={right}
-          initial={{ right: '-100dvw' }}
-          animate={{ right: right }}
-          exit={{ right: '-100dvw' }}
+          bottom={bottom}
+          isMobile={isMobile}
+          initial={isMobile ? { bottom: '-100dvh' } : { right: '-100dvw' }}
+          animate={isMobile ? { bottom: bottom } : { right: right }}
+          exit={isMobile ? { bottom: '-100dvh' } : { right: '-100dvw' }}
           transition={{ duration: 1, ease: [0.4, 0, 0.2, 1] }}
         >
           {error ? (
-            <ErrorContainer>
+            <S.ErrorContainer>
               <div>에러가 발생했습니다: {error.message}</div>
-            </ErrorContainer>
+            </S.ErrorContainer>
           ) : isLoading ? (
-            <LoadingContainer>
+            <S.LoadingContainer>
               <div>로딩 중...</div>
-            </LoadingContainer>
+            </S.LoadingContainer>
           ) : !interview ? (
-            <ErrorContainer>
+            <S.ErrorContainer>
               <div>인터뷰를 찾을 수 없습니다.</div>
-            </ErrorContainer>
+            </S.ErrorContainer>
           ) : (
             <>
-              <DetailPageName>{interview?.stores.name}</DetailPageName>
-              <InterviewHeader>
-                <InterviewTitle>
-                  <InterviewStore data-text={interview?.stores.name}>{interview?.stores.name}</InterviewStore>
-                  <InterviewPerson>{interview?.stores.person}</InterviewPerson>
-                  <InterviewIntro>{interview?.intro}</InterviewIntro>
-                </InterviewTitle>
+              <S.DetailPageName>{interview?.stores.name}</S.DetailPageName>
 
-                <InterviewCoverImg src={interview?.cover_img} alt="이미지" />
-              </InterviewHeader>
+              <S.InterviewHeader>
+                <S.InterviewTitle>
+                  <S.InterviewStore data-text={interview?.stores.name}>{interview?.stores.name}</S.InterviewStore>
+                  <S.InterviewPerson>{interview?.stores.person}</S.InterviewPerson>
+
+                </S.InterviewTitle>
+                <S.InterviewIntro>{interview?.intro}</S.InterviewIntro>
+                <S.InterviewCoverImg src={interview?.cover_img} alt="이미지" />
+              </S.InterviewHeader>
+
               <EditorInterviewRender item={interview?.contents} />
-              <InterviewInfo>{interview?.date}</InterviewInfo>
-              <InterviewInfo>{interview?.interviewee}</InterviewInfo>
+              <S.InterviewLink> {interview?.stores.name} {interview?.stores.person} 기술자와 <br />산림동의 다른 기술자들의 이야기를 <br /><span style={{ fontWeight: '900', padding: '8px 0px', color: 'rgb(71, 71, 71)' }}> &lt;산림동의 만드는 사람들&gt;</span> 책에서 계속 만나 보실 수 있습니다<Link href='https://tumblbug.com/homofaber' target='_blank'> 구매 링크 바로가기</Link></S.InterviewLink>
             </>
           )}
 
           {/* 커스텀 스크롤바 */}
-          <CustomScrollbar
-            scrollState={scrollState}
-            onScrollToRatio={scrollToRatio}
-          />
-        </DetailWrapper>
+          {!isMobile && (
+            <CustomScrollbar
+              scrollState={scrollState}
+              onScrollToRatio={scrollToRatio}
+            />
+          )}
+        </S.DetailWrapper>
       )}
     </AnimatePresence>
   );
